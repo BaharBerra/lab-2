7f7f006d6a73e6e1fdde63e9b63bf32467fc09e3
201-6 <201-6@DESKTOP-Q9A3KCC>
2023-02-27 17:25:27 +0300

    Proje dosyası ekle.

---
+++ lab 2/lab 2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+  {
+    int x,n ;
+    double y =0;
+
+    cout << "Input n>1, n=";
+    cin >> n;
+    cout << "Input x = ";
+    cin >> x;
+
+    float result;
+
+    if (x <= 0) {
+        float prod = 1;
+        for (double i = 0; i < n-1 ; i++) {
+            prod *= (pow(i, 2) + i);
+        }
+        result = prod;
+    }
+    else {
+        float sum = 0;
+        for (double i = 0; i < n-1 ; i++) {
+            for (double j = 0; j < n-1; j++) {
+                sum += (x / (i + j));
+            }
+        }
+        result = sum;
+    }
+
+    cout <<"x= "<< x << "; "<< "y= "<< y << "Result: " << result << endl;
+
+    return 0;
+}
